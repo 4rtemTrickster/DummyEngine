026b962bc5202875d82ed2cf11bb3703a44198fd
4rtemTrickster <korneev.arty@yandex.ru>
2021-12-15 16:19:35 +0300

    bruh 2

--- DummyEngine/src/Dummy/Log/Log.h
+++ DummyEngine/src/Dummy/Log/Log.h
@@ -13,8 +13,8 @@ namespace Dummy
     public:
         static void Init();
 
-        static Ref<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
-        static Ref<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+        static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+        static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
 
     private:
 
