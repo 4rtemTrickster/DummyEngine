cae2c81013d9cb175f56a9fcff8792b6757f482c
4rtemTrickster <korneev.arty@yandex.ru>
2022-01-05 20:06:32 +0300

    and another one

--- DummyEngine/src/Platform/OpenGL/Renderer/GraphicsContext/OpenGLContext.cpp
+++ DummyEngine/src/Platform/OpenGL/Renderer/GraphicsContext/OpenGLContext.cpp
@@ -24,6 +24,15 @@ namespace Dummy
         DE_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
         DE_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
         DE_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
+
+#ifdef DE_ENABLE_ASSERTS
+        int versionMajor;
+        int versionMinor;
+        glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+        glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+        DE_CORE_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "Dummy requires at least OpenGL version 4.5!");
+#endif
     }
 
     void OpenGLContext::SwapBuffers()
